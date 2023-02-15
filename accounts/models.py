699c0c17262f209220bb5d14fbff4dfc8ef57b68
Mustafanaji0413 <mustafanaji0413@gmail.com>
2023-02-15 19:46:46 +0000

    Add the functionality and requried fields to create a super user

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
+
 class MyAccountManager(BaseUserManager):
     def create_user(self, first_name, last_name, username, email, password=None):
         if not email:
@@ -15,6 +16,26 @@ class MyAccountManager(BaseUserManager):
             first_name=first_name,
             last_name=last_name,
         )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, first_name, last_name, email, username, password):
+        user = slef.create_user(
+            email=self.normalize_email(email),
+            username=username,
+            password=password,
+            first_name=first_name,
+            last_name=last_name,
+        )
+        user.is_admin = True
+        user.is_active = True
+        user.is_staff = True
+        user.is_superadmin = True
+        user.save(using=self._db)
+        return user
+
 
 class account(AbstractBaseUser):
     first_name = models.CharField(max_length=50)
