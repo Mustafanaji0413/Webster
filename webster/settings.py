3de2e7bfb587983a50d7a691dff972f2497b6f81
Mustafanaji0413 <mustafanaji0413@gmail.com>
2023-02-15 18:14:24 +0000

    Load homes template and static logo

--- webster/settings.py
+++ webster/settings.py
@@ -118,6 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR/"static"
+STATICFILES_DIRS = [
+    'webster/static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
