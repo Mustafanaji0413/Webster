f2ad41c7b2bc6669d8c0cf1be15e56a087039922
Mustafanaji0413 <mustafanaji0413@gmail.com>
2023-02-16 17:09:02 +0000

    Implement filter by category functionality

--- store/views.py
+++ store/views.py
@@ -1,13 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
+from category.models import category
 
 # Create your views here.
 
 
-def store(request):
-    products = Product.objects.all().filter(is_avalible=True)
+def store(request, category_slug=None):
+    categories = None
+    products = None
+
+    if category_slug != None:
+        categories = get_object_or_404(category, slug=category_slug)
+        Products = Product.objects.filter(category=categories, is_avalible=True)
+        product_count = Products.count()
+    else:
+        products = Product.objects.all().filter(is_avalible=True)
+        product_count = products.count()
 
     context = {
         'products': products,
+        'product_count': product_count,
     }
     return render(request, 'store/store.html', context)
