53403f291b802ee3c90e0ed6a96b313a834d0abb
Mustafanaji0413 <mustafanaji0413@gmail.com>
2023-02-17 16:22:34 +0000

    Make the product detail info dynamic

--- store/views.py
+++ store/views.py
@@ -11,8 +11,8 @@ def store(request, category_slug=None):
 
     if category_slug != None:
         categories = get_object_or_404(category, slug=category_slug)
-        Products = Product.objects.filter(category=categories, is_avalible=True)
-        product_count = Products.count()
+        products = Product.objects.filter(category=categories, is_avalible=True)
+        product_count = products.count()
     else:
         products = Product.objects.all().filter(is_avalible=True)
         product_count = products.count()
@@ -25,4 +25,13 @@ def store(request, category_slug=None):
 
 
 def product_detail(request, category_slug, product_slug):
-    return render(request, 'store/product_detail.html')
+    try:
+        single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+        in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product).exists()
+    except Exception as e:
+        raise e
+
+    context = {
+        'single_product': single_product,
+    }
+    return render(request, 'store/product_detail.html', context)
