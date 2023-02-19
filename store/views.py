a869edd66cd367a17dd472098a9811e1d3c57b76
Mustafanaji0413 <mustafanaji0413@gmail.com>
2023-02-19 17:18:43 +0000

    Implement search functionlity

--- store/views.py
+++ store/views.py
@@ -3,7 +3,10 @@ from .models import Product
 from category.models import category
 from carts.views import _cart_id
 from carts.models import CartItem
+from django.db.models import Q
+
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.http import HttpResponse
 
 
 # Create your views here.
@@ -21,7 +24,7 @@ def store(request, category_slug=None):
         paged_products = paginator.get_page(page)
         product_count = products.count()
     else:
-        products = Product.objects.all().filter(is_avalible=True)
+        products = Product.objects.all().filter(is_avalible=True).order_by('id')
         paginator = Paginator(products, 6)
         page = request.GET.get('page')
         paged_products = paginator.get_page(page)
@@ -46,3 +49,16 @@ def product_detail(request, category_slug, product_slug):
         'in_cart': in_cart,
     }
     return render(request, 'store/product_detail.html', context)
+
+
+def search(request):
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            products = Product.objects.order_by('-created_date').filter(Q(description__icontains=keyword) | Q(product_name__icontains=keyword))   
+            product_count = products.count() 
+    context = {
+        'products': products,
+        'product_count': product_count
+    }
+    return render(request, 'store/store.html', context)
