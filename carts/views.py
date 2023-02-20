7fadfef4e4c7644c2141b533d516d09506a87301
Mustafanaji0413 <mustafanaji0413@gmail.com>
2023-02-20 15:31:50 +0000

    Added choose size and color on products

--- carts/views.py
+++ carts/views.py
@@ -15,10 +15,11 @@ def _cart_id(request):
 
 
 def add_cart(request, product_id):
-    color = request.GET['color']
-    size = request.GET['size']
-    return HttpResponse(color + ' ' + size)
-    exit()
+    if request.method == 'POST':
+
+        color = request.POST['color']
+        size = request.POST['size']
+        print(color, size)
 
     product = Product.objects.get(id=product_id)  # get the product 
     try:
