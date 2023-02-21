dc7957f4455b5ae7ac4ec6535182cd6291d5bd7d
Mustafanaji0413 <mustafanaji0413@gmail.com>
2023-02-21 16:52:49 +0000

    Adding user login and logout funcionality

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
 from .forms import RegistrationForm
 from .models import account
-from django.contrib import messages
+from django.contrib import messages, auth
 from random import randint
+from django.contrib.auth.decorators import login_required
 
 
 def register(request):
@@ -29,8 +30,24 @@ def register(request):
 
 
 def login(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+
+        user = auth.authenticate(email=email, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            #messages.success(request, 'You are now logged in!')
+            return redirect('home')
+        else:
+            messages.error(request, 'Invalid login information')
+            return redirect('login')
     return render(request, 'accounts/login.html')
 
 
+@login_required(login_url='login')
 def logout(request):
-    return 
+    auth.logout(request)
+    messages.success(request, 'You are logged out')
+    return redirect('register')
