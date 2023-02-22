94a7517ee7574e6437d1604ca2c0999b7dab1f9b
Mustafanaji0413 <mustafanaji0413@gmail.com>
2023-02-22 21:09:56 +0000

    Adding the basic functionlity and buttons and design of the checkout page

--- carts/views.py
+++ carts/views.py
@@ -116,3 +116,30 @@ def cart(request, total=0, quantity=0, cart_item=None):
         'grand_total': grand_total,
     }
     return render(request, 'store/cart.html', context)
+
+
+def checkout(request, total=0, quantity=0, cart_items=None):
+    try:
+        tax = 0
+        grand_total = 0
+        if request.user.is_authenticated:
+            cart_items = CartItem.objects.filter(user=request.user, is_active=True)
+        else:
+            cart = Cart.objects.get(cart_id=_cart_id(request))
+            cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+        for cart_item in cart_items:
+            total += (cart_item.product.price * cart_item.quantity)
+            quantity += cart_item.quantity
+        tax = (2 * total)/100
+        grand_total = total + tax
+    except ObjectDoesNotExist:
+        pass #just ignore
+
+    context = {
+        'total': total,
+        'quantity': quantity,
+        'cart_items': cart_items,
+        'tax'       : tax,
+        'grand_total': grand_total,
+    }
+    return render(request, 'store/checkout.html', context)
