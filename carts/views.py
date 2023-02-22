b66990f39a3913e0f0494335baeb226d25aa5c67
Mustafanaji0413 <mustafanaji0413@gmail.com>
2023-02-22 21:58:11 +0000

    Modifying cart counter and cart view to handle logged in users

--- carts/views.py
+++ carts/views.py
@@ -3,6 +3,7 @@ from store.models import Product, Variation
 from .models import Cart, CartItem
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -95,19 +96,23 @@ def remove_cart_item(request, product_id, cart_item_id):
     return redirect('cart')
 
 
-def cart(request, total=0, quantity=0, cart_item=None):
+def cart(request, total=0, quantity=0, cart_items=None):
     try:
         tax = 0
         grand_total = 0
-        cart = Cart.objects.get(cart_id=_cart_id(request))
-        cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+        if request.user.is_authenticated:
+            cart_items = CartItem.objects.filter(user=request.user, is_active=True)
+        else:
+            cart = Cart.objects.get(cart_id=_cart_id(request))
+            cart_items = CartItem.objects.filter(cart=cart, is_active=True)
         for cart_item in cart_items:
             total += (cart_item.product.price * cart_item.quantity)
             quantity += cart_item.quantity
         tax = (2 * total)/100
         grand_total = total + tax
     except ObjectDoesNotExist:
-        pass
+        pass #just ignore
+
     context = {
         'total': total,
         'quantity': quantity,
