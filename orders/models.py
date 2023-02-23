fa5e6ced9cfa7d121ad86cbad3df3ba1c923bdfd
Mustafanaji0413 <mustafanaji0413@gmail.com>
2023-02-23 01:37:16 +0000

    Rendering out user info and products in car so that the user can review order before making the payment

--- orders/models.py
+++ orders/models.py
@@ -28,7 +28,7 @@ class Order(models.Model):
     order_number = models.CharField(max_length=20)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    phone = models.CharField(max_length=15)
+    phone = models.CharField(max_length=15) 
     email = models.EmailField(max_length=50)
     address_line_1 = models.CharField(max_length=50)
     address_line_2 = models.CharField(max_length=50, blank=True)
@@ -65,6 +65,12 @@ class OrderProduct(models.Model):
     ordered = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def full_name(self):
+        return f'{self.first_name} {self.last_name}'
+
+    def full_address(self):
+        return f'{self.address_line_1} {self.address_line_2}'
 
     def __str__(self):
         return self.product.product_name
