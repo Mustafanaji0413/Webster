c537498df8e7f846c45baf68f53599be64b229fe
Mustafanaji0413 <mustafanaji0413@gmail.com>
2023-02-23 20:43:29 +0000

    Displaying the star rating in the reviews

--- accounts/models.py
+++ accounts/models.py
@@ -57,6 +57,9 @@ class account(AbstractBaseUser):
 
     object = MyAccountManager()
 
+    def full_name(self):
+        return f'{self.first_name} {self.last_name}'
+
     def __str__(self):
         return self.email
 
--- store/views.py
+++ store/views.py
@@ -48,15 +48,18 @@ def product_detail(request, category_slug, product_slug):
         raise e
 
     try:
-        orderproduct = orderProduct.objects.filter(user=request.user, product_id=single_product.id).exists()
+        orderproduct = OrderProduct.objects.filter(user=request.user, product_id=single_product.id).exists()
     except OrderProduct.DoesNotExist:
         orderproduct = None
 
+       # Get the reviews
+    reviews = ReviewRating.objects.filter(product_id=single_product.id, status=True)
 
     context = {
         'single_product': single_product,
         'in_cart': in_cart,
         'orderproduct': orderproduct,
+        'reviews': reviews,
     }
     return render(request, 'store/product_detail.html', context)
 
