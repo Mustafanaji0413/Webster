b27a55f103c9f488383dd22abcc149930276ea7c
Mustafanaji0413 <mustafanaji0413@gmail.com>
2023-02-23 01:15:05 +0000

    Order page setup

--- orders/views.py
+++ orders/views.py
@@ -6,6 +6,10 @@ import datetime
 from .models import Order
 
 
+def payments(request):
+    return render(request, 'orders/payments.html')
+
+
 def place_order(request, total=0, quantity=0,):
     current_user = request.user
 
