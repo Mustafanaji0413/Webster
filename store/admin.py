37a5c1ed3614bb7a6a3211f2f143fa479d900dc3
Mustafanaji0413 <mustafanaji0413@gmail.com>
2023-02-24 00:32:39 +0000

    Add product gallery models and enable to preview the images with django thumbnails

--- store/admin.py
+++ store/admin.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
-from .models import Product, Variation, ReviewRating
+from .models import Product, Variation, ReviewRating, ProductGallery
+import admin_thumbnails
 
-# Register your models here.
+
+class ProductGalleryInline(admin.TabularInline):
+    models = ProductGallery
+    extra = 1
 
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('product_name', 'price', 'stock', 'category', 'modified_date', 'is_avalible')
     prepopulated_fields = {'slug': ('product_name',)}
+    inlines = [ProductGalleryInline]
 
 
 class VariationAdmin(admin.ModelAdmin):
@@ -18,3 +23,4 @@ class VariationAdmin(admin.ModelAdmin):
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Variation, VariationAdmin)
 admin.site.register(ReviewRating)
+admin.site.register(ProductGallery)
