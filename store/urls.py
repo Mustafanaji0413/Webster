bb2f2fe0717d0ae7247c9ba2fd9599ae5fc2c141
Mustafanaji0413 <mustafanaji0413@gmail.com>
2023-02-25 18:04:50 +0000

    Add delete product functionality

--- store/urls.py
+++ store/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('submit_review/<int:product_id>/', views.submit_review, name='submit_review'),
     path('add/', views.add_product, name='add_product'),
     path('edit/', views.edit_product, name='edit_product'),
+    path('delete/', views.delete_product, name='delete_product'),
 
 ]
