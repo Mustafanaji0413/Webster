1fdb35299e544eabf17278f7abddd0cd44cd0d34
Mustafanaji0413 <mustafanaji0413@gmail.com>
2023-03-01 16:25:54 +0000

    setting debug to false

--- webster/settings.py
+++ webster/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['webster0413.herokuapp.com', 'localhost']
 
